fc739f8f360b1d7d4716504dcade8bb28e6c3950
onexsan <onexsan@gmail.com>
2024-09-20 12:08:07 +0400

    Добавляет inversify и контейнер с зависимостями в main.rest.ts

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -6,3 +6,4 @@ export { RentType } from './rent-type.enum.js';
 export { User } from './user.interface.js';
 export { UserType } from './user-type.enum.js';
 export { MockServerData } from './mock-server-data.interface.js';
+export { Component } from './component.enum.js';
