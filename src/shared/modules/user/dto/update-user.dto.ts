1bf76c2d3d588df74259d776a0e615f2fc0a1152
onexsan <onexsan@gmail.com>
2024-10-08 22:10:33 +0400

    Обновляет UpdateUserDto

--- src/shared/modules/user/dto/update-user.dto.ts
+++ src/shared/modules/user/dto/update-user.dto.ts
@@ -1,4 +1,9 @@
+import { UserType } from '../../../types/user-type.enum.js';
+
 export class UpdateUserDto {
+  public email?: string;
   public avatar?: string;
   public name?: string;
+  public type?: UserType;
+  public password?: string;
 }
