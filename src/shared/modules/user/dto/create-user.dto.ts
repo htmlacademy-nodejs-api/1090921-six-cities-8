b211ce7390ca03199defa22d2d05c36ec7b6d8f4
onexsan <onexsan@gmail.com>
2024-11-03 23:44:44 +0400

    Обновляет константы

--- src/shared/modules/user/dto/create-user.dto.ts
+++ src/shared/modules/user/dto/create-user.dto.ts
@@ -3,6 +3,7 @@ import { IsEmail, IsString, Length, IsEnum } from 'class-validator';
 import { CREATE_USER_MESSAGES } from './create-user.messages.js';
 
 import { UserType } from '../../../types/user-type.enum.js';
+import { USER_RESTRICTIONS } from '../user.constants.js';
 
 export class CreateUserDTO {
   @IsEmail({}, { message: CREATE_USER_MESSAGES.EMAIL.INVALID_FORMAT })
@@ -12,13 +13,13 @@ export class CreateUserDTO {
   public avatar?: string;
 
   @IsString({ message: CREATE_USER_MESSAGES.NAME.INVALID_FORMAT })
-  @Length(1, 15, { message: CREATE_USER_MESSAGES.NAME.LENGTH_FIELD })
+  @Length(USER_RESTRICTIONS.NAME.MIN_LENGTH, USER_RESTRICTIONS.NAME.MAX_LENGTH, { message: CREATE_USER_MESSAGES.NAME.LENGTH_FIELD })
   public name: string;
 
   @IsEnum(UserType, { message: CREATE_USER_MESSAGES.TYPE.INVALID_FORMAT })
   public type: UserType;
 
   @IsString({ message: CREATE_USER_MESSAGES.PASSWORD.INVALID_FORMAT })
-  @Length(6, 12, { message: CREATE_USER_MESSAGES.PASSWORD.LENGTH_FIELD })
+  @Length(USER_RESTRICTIONS.PASSWORD.MIN_LENGTH, USER_RESTRICTIONS.PASSWORD.MAX_LENGTH, { message: CREATE_USER_MESSAGES.PASSWORD.LENGTH_FIELD })
   public password: string;
 }
